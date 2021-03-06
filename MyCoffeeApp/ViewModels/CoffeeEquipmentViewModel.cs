d44b3bca6ea172edc963a5f2ccc8b4087acb8f5d
Mwansa Pembanyali <mwansap07@gmail.com>
2021-03-06 23:47:36 +0200

    feat: added Coffee model and some mock Coffee data to the CoffeeEquipmentViewModel class

--- MyCoffeeApp/ViewModels/CoffeeEquipmentViewModel.cs
+++ MyCoffeeApp/ViewModels/CoffeeEquipmentViewModel.cs
@@ -1,33 +1,43 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using MvvmHelpers;
 using MvvmHelpers.Commands;
+using MyCoffeeApp.Models;
 
 namespace MyCoffeeApp.ViewModels
 {
     public class CoffeeEquipmentViewModel : ViewModelBase
     {
+        public ObservableRangeCollection<Coffee> Coffee { get; set; }
+        public ObservableRangeCollection<Grouping<string, Coffee>> CoffeeGroups { get; }
+
+        public AsyncCommand RefreshCommand { get; }
         public CoffeeEquipmentViewModel()
         {
-            IncreaseCount = new Command(OnIncrease);
             Title = "Coffee Equipment";
+
+            Coffee = new ObservableRangeCollection<Coffee>();
+            CoffeeGroups = new ObservableRangeCollection<Grouping<string, Coffee>>();
+
+            var image = "https://www.yesplz.coffee/app/uploads/2020/11/emptybag-min.png";
+
+            Coffee.Add(new Coffee { Roaster = "Yes Plz", Name = "Sip of Sunshine", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Yes Plz", Name = "Potent Potable", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Blue Bottle", Name = "Kenya Kiambu Handege", Image = image });
+
+            CoffeeGroups.Add(new Grouping<string, Coffee>("Blue Bottle", new[] { Coffee[2] }));
+            CoffeeGroups.Add(new Grouping<string, Coffee>("Yes Plz", Coffee.Take(2)));
+
+            RefreshCommand = new AsyncCommand(Refresh);
         }
 
-        public ICommand IncreaseCount { get; }
-
-        private void OnIncrease(object obj)
+        async Task Refresh()
         {
-            count++;
-            CountDisplay = $"You clicked {count} time(s)";
-        }
-
-        int count = 0;
-
-        string countDisplay = "Click Me!";
-        public string CountDisplay
-        {
-            get => countDisplay;
-            set => SetProperty(ref countDisplay, value);
+            IsBusy = true;
+            await Task.Delay(2000);
+            IsBusy = false;
         }
     }
 }
