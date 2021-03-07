d9fc8714ceddbe6810fca2a72b8ded65217443f7
Mwansa Pembanyali <mwansap07@gmail.com>
2021-03-07 20:45:08 +0200

    feat: added features in ListView such as refresh and tapped events.

--- MyCoffeeApp/Views/CoffeeEquipmentPage.xaml.cs
+++ MyCoffeeApp/Views/CoffeeEquipmentPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows.Input;
+using MyCoffeeApp.Models;
 using Xamarin.Forms;
 
 namespace MyCoffeeApp.Views
@@ -10,6 +11,29 @@ namespace MyCoffeeApp.Views
         public CoffeeEquipmentPage()
         {
             InitializeComponent();
+        }
+
+        async void ListView_ItemSelected(System.Object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        {
+            var coffee = ((ListView)sender).SelectedItem as Coffee;
+            if (coffee == null)
+                return;
+
+            await DisplayAlert("Coffee Selected", coffee.Name, "OK");
+        }
+
+        void ListView_ItemTapped(System.Object sender, Xamarin.Forms.ItemTappedEventArgs e)
+        {
+            ((ListView)sender).SelectedItem = null;
+        }
+
+        async void MenuItem_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var coffee = ((MenuItem)sender).BindingContext as Coffee;
+            if (coffee == null)
+                return;
+
+            await DisplayAlert("Coffee Favorited", coffee.Name, "OK");
         }
     }
 }
