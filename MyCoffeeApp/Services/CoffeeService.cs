0c89f154f2aa75348ad91aabf358f1bdd3fbd123
Mwansa Pembanyali <mwansap07@gmail.com>
2021-03-09 23:03:46 +0200

    feat: added sqlite-net-pcl package. Added CoffeeService service with scaffolded methods

---
+++ MyCoffeeApp/Services/CoffeeService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MyCoffeeApp.Services
+{
+    public class CoffeeService
+    {
+        static async Task Init()
+        {
+
+        }
+
+        public static async Task AddCoffee(string name, string roaster)
+        {
+
+        }
+
+        public static async Task RemoveCoffee(int id)
+        {
+
+        }
+
+        public static async Task GetCoffee()
+        {
+
+        }
+    }
+}
