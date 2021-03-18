0f8f7c2c2e047d8f5513f2c1d8cc962966539969
Mwansa Pembanyali <mwansap07@gmail.com>
2021-03-19 01:23:53 +0200

    feta: added CRUD methods for WebAPI

--- MyCoffee.WebAPI/Controllers/CoffeeController.cs
+++ MyCoffee.WebAPI/Controllers/CoffeeController.cs
@@ -1,10 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using MyCoffeeApp.Models;
+
 namespace MyCoffee.WebAPI.Controllers
 {
-    public class CoffeeController
+    public class CoffeeController : ControllerBase
     {
-        public CoffeeController()
+        public static List<Coffee> Coffee { get; } = new List<Coffee>();
+
+        [HttpGet]
+        public IEnumerable<Coffee> Get()
         {
+            return Coffee;
+        }
+
+        [HttpGet("{id}")]
+        public Coffee Get(int id)
+        {
+            return Coffee.FirstOrDefault(c => c.Id == id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Coffee value)
+        {
+            Coffee.Add(value);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Coffee value)
+        {
+            var coffee = Coffee.FirstOrDefault(c => c.Id == id);
+            if (coffee == null)
+            {
+                return;
+            }
+
+            coffee = value;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var coffee = Coffee.FirstOrDefault(c => c.Id == id);
+            if (coffee == null)
+            {
+                return;
+            }
+
+            Coffee.Remove(coffee);
         }
     }
 }
