e36af7a5930c9b3428477cb89dd97c29f98cfe59
Mwansa Pembanyali <mwansap07@gmail.com>
2021-03-19 01:42:51 +0200

    feat: Added InternetCoffee viewmodel and page

---
+++ MyCoffeeApp/Views/InternetCoffeePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MyCoffeeApp.Views
+{
+    public partial class InternetCoffeePage : ContentPage
+    {
+        public InternetCoffeePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
